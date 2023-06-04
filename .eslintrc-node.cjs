b268ce3f563c8bd83cb2d4566ac7c4953a7ff709
mAdutskevich <madutskevich@gmail.com>
2023-06-04 22:37:24 +0200

    add: node eslint config

--- .eslintrc-node.cjs
+++ .eslintrc-node.cjs
@@ -6,9 +6,7 @@ module.exports = {
     extends: [
         'airbnb',
         'airbnb-typescript',
-        'airbnb/hooks',
         'eslint:recommended',
-        'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
         'eslint-config-prettier',
@@ -22,15 +20,9 @@ module.exports = {
         },
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: ['../tsconfig.eslint.json'],
-        tsconfigRootDir: __dirname,
+        project: ['tsconfig.json'],
     },
-    settings: {
-        'import/resolver': {
-            typescript: {},
-        },
-    },
-    plugins: ['react', '@typescript-eslint', 'jsx-expressions'],
+    plugins: ['@typescript-eslint', 'jsx-expressions'],
     rules: {
         'jsx-expressions/strict-logical-expressions': 'error',
         quotes: ['error', 'single'],
@@ -83,46 +75,6 @@ module.exports = {
         '@typescript-eslint/array-type': 0,
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/no-object-literal-type-assertion': 0,
-        'react/prop-types': 'off',
-        'react/jsx-indent': ['error', 4],
-        'react/jsx-indent-props': ['error', 4],
-        'react/jsx-one-expression-per-line': 0,
-        'react/prefer-stateless-function': [
-            0,
-            {
-                ignorePureComponents: true,
-            },
-        ],
-        'react/forbid-prop-types': 0,
-        'react/jsx-no-undef': [
-            2,
-            {
-                allowGlobals: true,
-            },
-        ],
-        'react/jsx-no-useless-fragment': [
-            'error',
-            {
-                allowExpressions: true,
-            },
-        ],
-        'react/destructuring-assignment': 0,
-        'react/no-did-update-set-state': 0,
-        'react/sort-comp': 0,
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 0,
-        'react/jsx-filename-extension': [
-            1,
-            {
-                extensions: ['.jsx', '.tsx'],
-            },
-        ],
-        'react/no-array-index-key': 0,
-        'react/jsx-props-no-spreading': 'error',
-        'react/no-danger': 0,
-        'react/static-property-placement': 0,
-        'react/require-default-props': 0,
-        'react/button-has-type': 0,
         'jsx-a11y/media-has-caption': 0,
         'jsx-a11y/anchor-is-valid': 0,
         'jsx-a11y/interactive-supports-focus': 0,
@@ -151,13 +103,6 @@ module.exports = {
                     some: ['nesting', 'id'],
                 },
                 allowChildren: false,
-            },
-        ],
-        'react/function-component-definition': [
-            2,
-            {
-                namedComponents: 'arrow-function',
-                unnamedComponents: 'arrow-function',
             },
         ],
     },
